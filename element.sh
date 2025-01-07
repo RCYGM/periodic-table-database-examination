71b4d021e64e8b05f84840fc0c8e36427a5e5679
Jürgen  Müller <jurgenmuller997@gmail.com>
2025-01-07 09:40:51 +0000

    feat: make query database
    feat: Segmentation created
    
    feat: Variable creation
    
    feat: Creation of query by number
    
    feat: Creation of search by letter or word

--- element.sh
+++ element.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 if [[ ! $1 ]]; then
   echo "Please provide an element as an argument."
+  if [[ $1 =~ ^[0-9]+$ ]]; then
+    ELEMENT_DATA=$($PSQL "SELECT atomic_number, name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius  FROM elements INNER JOIN properties USING(atomic_number) WHERE atomic_number = $1;")
+  else
+    ELEMENT_DATA=$($PSQL "SELECT atomic_number, name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius  FROM elements INNER JOIN properties USING(atomic_number) WHERE name ILIKE '$1' OR symbol ILIKE '$1';")
+  fi
 fi
